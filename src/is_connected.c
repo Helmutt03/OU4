fc9f3010be9ff5ccdd777bf8132a213283854251
Helmutt03 <122628613+Helmutt03@users.noreply.github.com>
2026-03-16 11:38:23 +0100

    Added comments for is_connected.c

--- src/is_connected.c
+++ src/is_connected.c
@@ -1,6 +1,14 @@
 /**
  * @file is_connected.c
- * @brief something something something...
+ *
+ * @brief An interactive program used to determine if there is a path from one aiport to the
+ *        other. Takes as input a map file containing the routes of the airports.
+ *        The file needs to follow the specification for the program to work correctly.
+ *
+ *        This program is made for the OU4 assignment of the "Datastructures and algorithms"
+ *        course at the Department of Computing Science, Umea University
+ *
+ * Duplicates are handled via the function load_graph()
  *
  * @authors: Helmer Nordström (ens24hnm@cs.umu.se)
  *           Otto Silander
@@ -20,7 +28,7 @@
 #include <queue.h>
 
 #define MAXNODENAME 40
-#define BUFSIZE 400
+#define BUFSIZE 400 // Used for reading input lines
 
 // Function declarations:
 void check_params(int argc, char *argv[], FILE **map_data);
@@ -35,11 +43,16 @@ bool line_is_comment(const char *s);
 int parse_map_line(const char *buf, char *n1, char *n2);
 
 /**
- * @brief
+ * @brief Main program loop, loads the graph,
+ *        asks the user for two airports and returns
+ *        if they are connected or not
  *
- * @param argc
- * @param argv
- * @return
+ * @param argc The amount of args the program was given,
+ *             used in verifying correct use of the program
+ * @param argv The argument strings,
+ *             used to read from the map file
+ *
+ * @return 0 on normal exit
  */
 int main(int argc, char *argv[]) {
 
@@ -57,11 +70,13 @@ int main(int argc, char *argv[]) {
 	node *src = NULL;
 	node *dest = NULL;
 
+	// flag used to indicate if the program should terminate
 	int quit = 0;
 
-	// Program loop
+	// main program loop
 	while (quit == 0) {
 
+		// Ask the user for input and load it
 		printf("Enter origin and destination (quit to exit): ");
 		fgets(buf, BUFSIZE, stdin);
 
@@ -76,16 +91,17 @@ int main(int argc, char *argv[]) {
 			// if one or more of the inputs doesnt exist
 			// dont try to find a path
 			if ((src = graph_find_node(g, n1)) == NULL || (dest = graph_find_node(g, n2)) == NULL) {
-				fprintf(stderr, "Please input correct airports\n\n");
+				printf("Usage: AIRPORT1 AIRPORT2\n\n");
 			}
+
 			// Try to find a path
 			else {
 				if (find_path(g, src, dest)) {
-					printf("There is a path from %s to %s\n\n", n1, n2);
+					printf("There is a path from %s to %s.\n\n", n1, n2);
 				}
 
 				else {
-					printf("There is no path from %s to %s\n\n", n1, n2);
+					printf("There is no path from %s to %s.\n\n", n1, n2);
 				}
 			}
 		}
@@ -106,19 +122,22 @@ int main(int argc, char *argv[]) {
 }
 
 /**
- * @brief
+ * @brief Check that we got the right amount of args
+ *        and can open the file given to read from
  *
- * @param argc
- * @param argv
- * @param map_data
+ * @param argc The number of args
+ * @param argv The file to be opened and read
+ * @param map_data The FILE pointer used for reading the file
  */
 void check_params(int argc, char *argv[], FILE **map_data) {
 
+	// Correct amount of arguments
 	if (argc != 2) {
-		printf("Please input correct file\n");
+		fprintf(stderr, "Please input correct file\n");
 		exit(EXIT_FAILURE);
 	}
 
+	// Open the file for reading
 	*map_data = fopen(argv[1], "r");
 	if (map_data == NULL) {
 		fprintf(stderr, "Could not open file %s\n", argv[1]);
@@ -127,41 +146,45 @@ void check_params(int argc, char *argv[], FILE **map_data) {
 }
 
 /**
- * @brief
+ * @brief Loads the graph structure from the given map data
  *
- * @param map_data
- * @return
+ * Handles duplicates by checking if they already exists in
+ * the graph before inserting nodes.
+ *
+ * @param map_data FILE pointer containing the map file
+ * @return The modified graph structure
  */
 graph *load_graph(FILE *map_data) {
 
 	// get the amount of routes to be loaded into the graph
 	int num_of_routes = get_num_of_routes(map_data);
 
-	// Every route will have two nodes
+	// Every route will have two nodes,
+	// all of which could be unique
 	graph *g = graph_empty(num_of_routes * 2);
 
+	// Allocate the needed variables for reading and assigning
 	char *buf = calloc(BUFSIZE, sizeof(*buf));
 	char *n1 = calloc(MAXNODENAME, sizeof(*n1));
 	char *n2 = calloc(MAXNODENAME, sizeof(*n2));
 
+	// Given the number of routes, read that many lines from the file
 	for (int i = 1; i <= num_of_routes; i++) {
 
-		// if we reach the EOF early the num_of_routes number was
+		// if we reach EOF early the num_of_routes variables was
 		// incorrect and the map file is incorrect
 		if (fgets(buf, BUFSIZE, map_data) == NULL) {
-			fprintf(stderr, "something wrong...\n");
+			fprintf(stderr, "Input file doesnt follow the specification\n");
 			exit(EXIT_FAILURE);
 		}
 
+		// We dont want to count this line
 		if (line_is_blank(buf) || line_is_comment(buf)) {
 			i--;
 		}
 		else {
 			// Read the current line and extract the route names
 			parse_map_line(buf, n1, n2);
-
-			// debug
-			printf("%s-%s\n", n1, n2);
 
 			// Only add the nodes if they aren't in the graph already
 			if (graph_find_node(g, n1) == NULL) {
@@ -171,10 +194,13 @@ graph *load_graph(FILE *map_data) {
 				g = graph_insert_node(g, n2);
 			}
 
+			// Add the edge between these two nodes
 			g = graph_insert_edge(g, graph_find_node(g, n1), graph_find_node(g, n2));
 		}
 	}
 
+	// Free the memory allocated and return
+	// the modified graph g
 	free(buf);
 	free(n1);
 	free(n2);
@@ -182,17 +208,26 @@ graph *load_graph(FILE *map_data) {
 }
 
 /**
- * @brief
+ * @brief Reads the file for a the number specifying
+ *        the amount of routes the file contains
  *
- * @param map_data
- * @return
+ * @param map_data FILE pointer to the map data
+ *                 which we are to read from
+ *
+ * @pre We assume that there will be no blank space
+ *      before the number in the file
+ *
+ * @return an integer with the number of routes in the file
  */
 int get_num_of_routes(FILE *map_data) {
-	// We assume that there will be no blank space before the number
+
+	// Statically allocated memory
 	char buf[BUFSIZE];
 	int num = 0;
 
 	while (num == 0) {
+		// if we reach EOF before reading a number on the first
+		// character of the line
 		if (fgets(buf, BUFSIZE, map_data) == NULL) {
 			fprintf(stderr, "Input file doesnt follow the specification\n");
 			exit(EXIT_FAILURE);
@@ -202,20 +237,22 @@ int get_num_of_routes(FILE *map_data) {
 		// number representing the number of routes
 		if (buf[0] >= '0' && buf[0] <= '9') {
 			sscanf(buf, "%d", &num);
-			printf("%s", buf);
 		}
 	}
 
+	// return the number found
 	return num;
 }
 
 /**
- * @brief
+ * @brief Uses a breadth-first search algorithm to determine
+ *        if there is a path connecting two nodes in the graph
  *
- * @param g
- * @param src
- * @param dest
- * @return
+ * @param g A graph structure
+ * @param src The airport we are to depart from
+ * @param dest The airport we try to reach
+ *
+ * @return true if we can reach dest from src, false if we cannot
  */
 bool find_path(graph *g, node *src, node *dest) {
 
@@ -226,6 +263,7 @@ bool find_path(graph *g, node *src, node *dest) {
 
 	// Breadth-first search
 	g = graph_node_set_seen(g, src, true);
+	// Add the src node first in the queue
 	queue *q = queue_enqueue(queue_empty(NULL), src);
 	node *n;
 	node *b;
@@ -233,8 +271,11 @@ bool find_path(graph *g, node *src, node *dest) {
 	dlist_pos p = NULL;
 	int found = 0;
 
+	// Loop through all nodes reachable via src or until we have found dest
 	while (!queue_is_empty(q) && found == 0) {
 
+		// Get the node first in the queue,
+		// delete it from the queue
 		n = queue_front(q);
 		q = queue_dequeue(q);
 
@@ -242,18 +283,24 @@ bool find_path(graph *g, node *src, node *dest) {
 		neighbours = graph_neighbours(g, n);
 		p = dlist_first(neighbours);
 
-		while (!dlist_is_end(neighbours, p)) {
+		// Loop over the neighbours until it we either find
+		// the destination or we run out of neighbours
+		while (!dlist_is_end(neighbours, p) && found == 0) {
 			b = dlist_inspect(neighbours, p);
 
 			if (nodes_are_equal(b, dest)) {
+				// Update the flag to end the loop
 				found = 1;
 			}
 			else {
+				// if this is a new node, make it seen
+				// and add it to the queue
 				if (!graph_node_is_seen(g, b)) {
 					graph_node_set_seen(g, b, true);
 					q = queue_enqueue(q, b);
 				}
 			}
+			// Update the position in the list
 			p = dlist_next(neighbours, p);
 		}
 		// free memory for neigbours
@@ -266,6 +313,7 @@ bool find_path(graph *g, node *src, node *dest) {
 	// Reset graph for next search
 	graph_reset_seen(g);
 
+	// Return the results
 	if (found == 1) {
 		return true;
 	}
